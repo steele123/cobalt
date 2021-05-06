1c1129ef25260a93e055cbc3abc2305583c9d173
Tyler Hill <tyler@oxide.fi>
2021-05-06 10:24:06 -0500

    Using an enclose macro to manage cloning lcu

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,15 @@ mod utils;
 
 mod process_worker;
 
+macro_rules! enclose {
+    ( ($( $x:ident ),*) $y:expr ) => {
+        {
+            $(let $x = $x.clone();)*
+            $y
+        }
+    };
+}
+
 fn main() -> eyre::Result<()> {
     toast::send("Trying to find the LeagueClient.exe process...")?;
 
@@ -46,39 +55,45 @@ fn main() -> eyre::Result<()> {
         utils::lcu::LCUClient::new(&lock_file_info.token, lock_file_info.port).unwrap(),
     ));
 
-    let lcu_clone = Arc::clone(&lcu);
-
     let rx = process_worker::spawn();
 
     println!("Controls\nCTRL+D to dodge your current champ select.\nCTRL+B to aram boost");
 
-    std::thread::spawn(move || {
+    std::thread::spawn(enclose! { (lcu) move || {
         let mut key_listener = KeyListener::new();
 
         key_listener
-            .register_hotkey(Modifiers::CTRL, Key::D, move || {
-                println!("Lobby Crash Queued...");
-                #[cfg(not(debug_assertions))]
-                lcu_clone.lock().unwrap().crash_lobby().unwrap();
-                #[cfg(debug_assertions)]
-                println!("Debug Assertions are on so you don't go into TFT");
-            })
+            .register_hotkey(
+                Modifiers::CTRL,
+                Key::D,
+                enclose! {(lcu) move || {
+                    println!("Lobby Crash Queued...");
+                    #[cfg(not(debug_assertions))]
+                    lcu.lock().unwrap().crash_lobby().unwrap();
+                    #[cfg(debug_assertions)]
+                    println!("Debug Assertions are on so you don't go into TFT");
+                }},
+            )
             .unwrap();
 
         key_listener
-            .register_hotkey(Modifiers::CTRL, Key::B, move || {
-                println!("ARAM Boost Queued...");
-                lcu_clone
-                    .lock()
-                    .unwrap()
-                    .send(&Endpoints::AramBoost, &Method::POST, "")
-                    .unwrap();
-                println!("ARAM Boost Completed...");
-            })
+            .register_hotkey(
+                Modifiers::CTRL,
+                Key::B,
+                enclose! {(lcu) move || {
+                    println!("ARAM Boost Queued...");
+                    lcu
+                        .lock()
+                        .unwrap()
+                        .send(&Endpoints::AramBoost, &Method::POST, "")
+                        .unwrap();
+                    println!("ARAM Boost Completed...");
+                }},
+            )
             .unwrap();
 
         key_listener.listen();
-    });
+    }});
 
     while let Ok(event) = rx.recv() {
         match event {
