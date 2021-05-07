53450d321796609f837521db4831494aa0023e94
Tyler Hill <tyler@oxide.fi>
2021-05-07 16:02:35 -0500

    Make message more clear.

--- src/main.rs
+++ src/main.rs
@@ -72,7 +72,7 @@ fn main() -> eyre::Result<()> {
                     #[cfg(debug_assertions)]
                     println!("Debug Assertions are on so you don't go into TFT");
 
-                println!("Lobby has been dodged, you can leave the TFT game ~45 seconds.");
+                println!("Lobby has been dodged, you can leave the TFT game after ~45 seconds.");
                 }},
             )
             .unwrap();
