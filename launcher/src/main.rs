e59ba054b74341c3710417dbea8f9c6bd4089d92
Steele Scott <60533996+steele123@users.noreply.github.com>
2021-05-08 00:30:54 -0400

    Kinda working launcher code? Also kinda retard

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+#![windows_subsystem = "windows"]
+
+use std::process::Command;
+
+use crate::fetcher::Fetcher;
+
+pub mod fetcher;
+
+fn main() -> eyre::Result<()> {
+    let version = Fetcher::get_version()?;
+
+    let tool_path = Fetcher::download_tool()?;
+
+    Command::new(tool_path).arg("--launcher").spawn()?;
+
+    Ok(())
+}
