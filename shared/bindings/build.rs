919b2961eaa8eda7b0eba63102fa29eb0b2926a6
Tyler Hill <tyhi@tyhi.rs>
2021-05-25 19:32:21 +0000

    Updated windows-rs to 0.10.0 [1 of 2]

--- shared/bindings/build.rs
+++ shared/bindings/build.rs
@@ -1,9 +1,10 @@
 fn main() {
     windows::build!(
-        Windows::Win32::WindowsAndMessaging::GetMessageW,
-        Windows::Win32::KeyboardAndMouseInput::{RegisterHotKey},
-        Windows::Win32::WindowsProgramming::CloseHandle,
-        Windows::Win32::SystemServices::{INVALID_HANDLE_VALUE, CreateProcessW, OpenProcess, TerminateProcess},
-        Windows::Win32::ToolHelp::{CreateToolhelp32Snapshot, PROCESSENTRY32, CREATE_TOOLHELP_SNAPSHOT_FLAGS, Process32First, Process32Next, MODULEENTRY32, Module32First},
+        Windows::Win32::UI::WindowsAndMessaging::GetMessageW,
+        Windows::Win32::UI::KeyboardAndMouseInput::{RegisterHotKey},
+        Windows::Win32::System::WindowsProgramming::CloseHandle,
+        Windows::Win32::System::Threading::{CreateProcessW, OpenProcess, TerminateProcess},
+        Windows::Win32::System::SystemServices::INVALID_HANDLE_VALUE,
+        Windows::Win32::System::Diagnostics::ToolHelp::{CreateToolhelp32Snapshot, PROCESSENTRY32, CREATE_TOOLHELP_SNAPSHOT_FLAGS, Process32First, Process32Next, MODULEENTRY32, Module32First},
     );
 }
