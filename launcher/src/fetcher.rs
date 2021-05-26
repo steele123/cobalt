87c6651e958e8e162ffbae8ae41851027906355d
Steele Scott <60533996+steele123@users.noreply.github.com>
2021-05-26 17:32:07 -0400

    Init

--- launcher/src/fetcher.rs
+++ launcher/src/fetcher.rs
@@ -1,104 +1,42 @@
 use std::{
     fs::File,
-    io::{Read, Write},
+    io::{Read, Seek, SeekFrom, Write},
     path::{Path, PathBuf},
 };
 
 use eyre::Result;
-use obfstr::obfstr;
-use xz2::read::{XzDecoder, XzEncoder};
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
+use tempfile::NamedTempFile;
+
+use crate::process;
 
 pub struct Fetcher;
 
 impl Fetcher {
-    pub fn get_version() -> Result<String> {
-        let resp = attohttpc::get(obfstr!("https://api.steele.gg/tools/cobalt/version"))
-            .header("Authorization", base64::encode("steele.gg"))
-            .send()?;
+    pub fn launch_tool() -> Result<()> {
+        let resp = attohttpc::get("https://github.com/steele123/cobalt/releases/latest/download/cobalt.exe").send()?;
 
-        if !resp.is_success() {
-            return Err(eyre::eyre!("Couldn't get version"));
-        }
+        let mut tempfile = NamedTempFile::new().unwrap();
 
-        Ok(resp.text()?)
-    }
+        let bytes = resp.bytes().unwrap();
 
-    pub fn get_cached_version() -> String {
-        let dir = get_version_dir();
+        tempfile.write_all(bytes.as_slice()).unwrap();
 
-        if !Path::new(dir.as_path()).exists() {
-            return "0.0.0".into();
-        }
+        tempfile.seek(SeekFrom::Start(0)).unwrap();
 
-        std::fs::read_to_string(dir.to_str().unwrap()).unwrap()
-    }
+        tempfile.close();
 
-    pub fn save_version(version: String) {
-        File::create(get_version_dir().as_path())
-            .unwrap()
-            .write_all(version.as_bytes())
-            .unwrap();
-    }
+        process::create_process(tempfile.path().to_str().unwrap().into())
+            .expect("Couldn't create a process from that path!");
 
-    pub fn get_tool(from_cache: bool) -> Result<String> {
-        if from_cache {
-            let mut dir = get_cache_dir();
-
-            dir.push(obfstr!("tool.exe"));
-
-            if !Path::new(dir.as_path()).exists() {
-                return download_tool();
-            }
-
-            return Ok(dir.to_str().unwrap().into());
-        }
-
-        download_tool()
+        Ok(())
     }
 }
 
-fn download_tool() -> Result<String> {
-    let resp = attohttpc::get(obfstr!("https://api.steele.gg/tools/cobalt.xz"))
-        .header("Authorization", base64::encode("steele.gg"))
-        .send()?;
-
-    let mut appdata_dir = get_cache_dir();
-
-    appdata_dir.push(obfstr!("tool.exe"));
-
-    let path = Path::new(appdata_dir.as_path());
-
-    let mut file = File::create(&path)?;
-
-    let input = resp.bytes().unwrap();
-
-    let mut bytes: Vec<u8> = Vec::new();
-
-    let mut decomp = XzDecoder::new(input.as_slice());
-
-    decomp.read_to_end(&mut bytes)?;
-
-    file.write_all(bytes.as_slice()).unwrap();
-
-    Ok(path.to_str().unwrap().into())
-}
-
-fn get_version_dir() -> PathBuf {
-    let mut cached_dir = get_cache_dir();
-
-    cached_dir.push(obfstr!("version"));
-
-    cached_dir
-}
-
-fn get_cache_dir() -> PathBuf {
-    let mut appdata_dir = dirs::cache_dir().unwrap();
-
-    appdata_dir.push(obfstr!("Cobalt"));
-
-    if !Path::new(appdata_dir.as_path()).exists() {
-        std::fs::create_dir(appdata_dir.clone()).unwrap();
-    }
-
-    appdata_dir
+fn rand(size: usize) -> String {
+    std::iter::repeat(())
+        .map(|()| thread_rng().sample(Alphanumeric))
+        .map(char::from)
+        .take(10)
+        .collect()
 }
--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -1,30 +1,13 @@
 #![windows_subsystem = "windows"]
 
 use fetcher::Fetcher;
-use win_utils::kill_process_by_name;
 
 pub mod fetcher;
 
 pub mod process;
 
 fn main() -> eyre::Result<()> {
-    let tool_path: String;
-
-    let cached_version = Fetcher::get_cached_version();
-
-    let latest_version = Fetcher::get_version().unwrap();
-
-    if cached_version == latest_version {
-        tool_path = Fetcher::get_tool(true).unwrap();
-    } else {
-        tool_path = Fetcher::get_tool(false).unwrap();
-    }
-
-    Fetcher::save_version(latest_version);
-
-    kill_process_by_name("Cobalt");
-
-    process::create_process(tool_path)?;
+    Fetcher::launch_tool().unwrap();
 
     Ok(())
 }
